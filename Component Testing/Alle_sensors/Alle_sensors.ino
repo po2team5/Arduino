2f46c107b3564abbb059af9779108fbc4bd9a76b
Elias Storme <elias.storme@gmail.com>
2016-02-29 11:44:26 +0100

    Initial Commit
    Samenstelling van alle lezingen van de sensoren.

---
+++ Component Testing/Alle_sensors/Alle_sensors.ino
@@ -0,0 +1,25 @@
+// Line sensor pins
+int lineEmitter = 2;
+int lineOne = 4;
+int lineTwo = 7;
+int lineThree = 8;
+int lineFour = 11;
+int lineFive = 12;
+int lineSix = 13;
+//Motor pins
+int motorA1 = 5;
+int motorA2 = 6;
+int motorB1 = 9;
+int motorB2 = 10;
+//Distance sensor pins
+int distancePin = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
