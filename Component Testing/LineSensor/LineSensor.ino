01197dde6ff5fdfefb552b0f713899cd0586f350
Elias Storme <elias.storme@gmail.com>
2016-03-14 18:50:15 +0100

    Sensor calibratie implementeren

--- Component Testing/LineSensor/LineSensor.ino
+++ Component Testing/LineSensor/LineSensor.ino
@@ -13,7 +13,25 @@ unsigned int sensorValues[NUM_SENSORS];
 void setup()
 {
   delay(500);
+  for (int i = 0; i<400; i++)
+  {
+    qtrrc.calibrate();
+  }
+  
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
+  for (int i = 0; i < NUM_SENSORS;i++)
+  {
+    Serial.print(qtrrc.calibratedMinimumOn[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(qtrrc.calibratedMaximumOn[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
   delay(1000);
 }
 
@@ -21,7 +39,7 @@ void setup()
 void loop()
 {
   // read raw sensor values
-  qtrrc.read(sensorValues);
+  unsigned int position = qtrrc.readLine(sensorValues);
 
   // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
   // 1023 means minimum reflectance
@@ -31,6 +49,7 @@ void loop()
     Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
   }
   Serial.println();
+  //Serial.println(position);
   
-  delay(250);
+  delay(1000);
 }
