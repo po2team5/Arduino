1c48643c45dc17f1f4120a0cb84b61ac4a48415e
Elias Storme <elias.storme@gmail.com>
2016-04-16 15:51:31 +0200

    testing without library

--- Component Testing/Motor/Motor.ino
+++ Component Testing/Motor/Motor.ino
@@ -1,16 +1,8 @@
 #include <Motor.h>
 
-Motor motorA = Motor(5,6);
-Motor motorB = Motor(9,10);
-
-
-
 void setup() {
-  motorA.setMotorSpeed(20);
-  motorB.setMotorSpeed(20);
-  motorA.startMotor();
-  motorB.startMotor();
-
+  analogWrite(6,50);
+  analogWrite(10,54);
 }
 
 void loop() {
